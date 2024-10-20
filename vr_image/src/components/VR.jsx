c0da08c517d9770e926cc0053cfd8510a8d78ae6
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2024-10-20 20:19:14 +0530

    design 1

--- vr_image/src/components/VR.jsx
+++ vr_image/src/components/VR.jsx
@@ -5,7 +5,6 @@ export default function VR({ VRImage }) {
     <div>
       <a-scene embedded>
         <a-sky src={VRImage}></a-sky>
-        <a-box position="0 1 -5" color="red"></a-box>
         <a-entity camera look-controls wasd-controls></a-entity>
       </a-scene>
     </div>

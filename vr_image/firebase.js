5d16f31d37d17b110b75fd567893d5dedfb14011
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-05-27 17:21:45 +0530

    env file

--- vr_image/firebase.js
+++ vr_image/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage"; // Import storage
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAeRcNQWe_3uLviyce2q1ktL_1TzU7nN4I",
-  authDomain: "vr-image-285ac.firebaseapp.com",
-  projectId: "vr-image-285ac",
-  storageBucket: "vr-image-285ac.appspot.com",
-  messagingSenderId: "589934469186",
-  appId: "1:589934469186:web:a7787a55641a34d2ee4ddb",
-  measurementId: "G-983N90R1XC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
